377b33a0585775a15eacaf8dcd653a69b61478e8
David Marques <davideliberto2001@gmail.com>
2024-09-21 19:29:49 -0300

    Melhorando meu css

--- paginas/projetos.js
+++ paginas/projetos.js
@@ -12,4 +12,20 @@
                 }, 1000); // Allow time for the opacity transition to complete
             }, 1000); // 3000 milliseconds = 3 seconds
           });
+
+          function toggleText() {
+            var dots = document.getElementById("dots");
+            var moreText = document.getElementById("more");
+            var btnText = document.getElementById("myBtn");
+        
+            if (dots.style.display === "none") {
+                dots.style.display = "inline";
+                btnText.innerHTML = "Ver mais";
+                moreText.style.display = "none";
+            } else {
+                dots.style.display = "none";
+                btnText.innerHTML = "Ver menos";
+                moreText.style.display = "inline";
+            }
+        }
           
